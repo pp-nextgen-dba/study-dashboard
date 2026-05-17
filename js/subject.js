3f8dc302d28b0c15dbdddf088fa8f430f1a92d93
pp-nextgen-dba <psi9999@gmail.com>
2026-05-17 18:07:58 +0800

    Update subject.js

--- js/subject.js
+++ js/subject.js
@@ -8,34 +8,368 @@ const defaultData = {
 
     chapters: [
 
-        {
-            form:"T2",
-            chapter:"Patterns+Seq",
-            chinese:"规律与数列",
-            status:"Not Started",
-            confidence:0,
-            last_updated:"2026-05-17"
-        },
+{
+form:"T2",
+chapter:"Patterns+Seq",
+chinese:"规律与数列",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
 
-        {
-            form:"T2",
-            chapter:"Factorisation",
-            chinese:"因式分解",
-            status:"Not Started",
-            confidence:0,
-            last_updated:"2026-05-17"
-        },
+{
+form:"T2",
+chapter:"Factorisation",
+chinese:"因式分解",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
 
-        {
-            form:"T4",
-            chapter:"Quadratics Functions",
-            chinese:"二次函数",
-            status:"Not Started",
-            confidence:0,
-            last_updated:"2026-05-17"
-        }
+{
+form:"T2",
+chapter:"Algegraic Formulae",
+chinese:"代数公式",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
 
-    ]
+{
+form:"T2",
+chapter:"Poligon",
+chinese:"多边形",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Circles",
+chinese:"圆",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"3D Geo",
+chinese:"三维几何",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Coor",
+chinese:"坐标",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Graphs of Func",
+chinese:"函数图像",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Speed + Accer",
+chinese:"速度与加速度",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Gradient OSL",
+chinese:"斜率与直线方程",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Isometric Transfm",
+chinese:"等距变换",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Measures OCT",
+chinese:"圆的测量",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T2",
+chapter:"Probability",
+chinese:"概率",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Indices",
+chinese:"指数",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Std Form",
+chinese:"标准形式",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Consumer: Investment",
+chinese:"消费数学：投资",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Scale Drawings",
+chinese:"比例绘图",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Trigo Ratios",
+chinese:"三角比",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Angles + Tangents of circles",
+chinese:"圆的角与切线",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Plans + elevations",
+chinese:"平面图与立体图",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"LOCI in 2 dimensions",
+chinese:"二维轨迹",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T3",
+chapter:"Straight Lines",
+chinese:"直线",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Quadratics Functions",
+chinese:"二次函数",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Bases",
+chinese:"数制",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Logical Reasoning",
+chinese:"逻辑推理",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Sets",
+chinese:"集合",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Graph Networks",
+chinese:"图论网络",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Linear Inequalities",
+chinese:"线性不等式",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Motion Graphs",
+chinese:"运动图像",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Ugrouped Data",
+chinese:"未分组数据",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Probability",
+chinese:"概率",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T4",
+chapter:"Consumer:Fin Mgmt",
+chinese:"消费数学：财务管理",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Variation",
+chinese:"变分",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Matrices",
+chinese:"矩阵",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Consumer: Insurance",
+chinese:"消费数学：保险",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Consumer:Tax",
+chinese:"消费数学：税务",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Congruency",
+chinese:"全等",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Ratios + trigo Func",
+chinese:"比率与三角函数",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Grouped Data",
+chinese:"分组数据",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+},
+
+{
+form:"T5",
+chapter:"Maths Modeling",
+chinese:"数学建模",
+status:"Not Started",
+confidence:0,
+last_updated:"2026-05-17"
+}
+
+]   
+    
 };
 
 
